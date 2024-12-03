32d231da639e0796032ccb1febba15006a54a29f
ParveenKhan <erparveenkhan@gmail.com>
2024-12-03 16:27:11 +0530

    Initial Commit

---
+++ ParveenDemoTests/ParveenDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParveenDemoTests.swift
+//  ParveenDemoTests
+//
+//  Created by ParveenKhan on 03/12/24.
+//
+
+import Testing
+@testable import ParveenDemo
+
+struct ParveenDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
